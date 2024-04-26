591a7d7d2843f82537743bc7a00c6890e56f792e
Yzak Carmo <yzak.carmo@gmail.com>
2024-04-25 21:19:12 -0300

    Implementei o Delete do User

--- src/main/java/com/yzak/spring_mongo/resources/UserResource.java
+++ src/main/java/com/yzak/spring_mongo/resources/UserResource.java
@@ -42,4 +42,10 @@ public class UserResource {
         return ResponseEntity.created(uri).build();
     }
 
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable String id) {
+        service.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
